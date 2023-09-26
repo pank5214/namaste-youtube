3c1768ed2bb42cd00670627fa57def39b06e60c6
pank5214 <pankajkumar5214@gmail.com>
2023-09-26 16:43:33 +0530

    added button on watchPage

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -6,17 +6,25 @@ const Subscribe = ({ videoId }) => {
   console.log("videosInfo", videosInfo);
   if (videosInfo.length === 0) return "Loading...";
   return (
-    <div className="mx-2 p-2">
-      <h1 className="font-extrabold">
+    <div className="mx-2 p-2 w-8/12">
+      <h1 className="font-extrabold text-xl">
         {videosInfo.find((videoInfo) => videoInfo.id === videoId).snippet.title}
       </h1>
-      <div>
-        <h3 className="font-medium my-2">
+      <div className="flex flex-wrap gap-x-80 items-center">
+        <h3 className="font-bold my-2">
           {
             videosInfo.find((videoInfo) => videoInfo.id === videoId).snippet
               .channelTitle
           }
         </h3>
+        <div className="flex gap-8">
+          <button className="px-5 p-2 m-2 bg-black text-white font-semibold rounded-full hover:bg-gray-300">
+            Join
+          </button>
+          <button className="px-5 p-2 m-2 bg-gray-300 text-black font-semibold rounded-full hover:bg-gray-400">
+            Subscribe
+          </button>
+        </div>
       </div>
     </div>
   );
