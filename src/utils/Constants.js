6c9560abceb23d7a3c99f02df9ebf68fc978d471
pank5214 <pankajkumar5214@gmail.com>
2023-10-12 16:44:56 +0530

    api key updated

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,8 +1,7 @@
 export const LIVE_CHAT_COUNT = 35;
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  process.env.REACT_APP_GOOGLE_API_KEY;
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyCW17mf7-nIuqV2wDOqhM0-xB2h9FdDkAk";
 
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
