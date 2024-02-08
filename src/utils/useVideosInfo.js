88e94de32ea42ef6e73dbeff14ab0a07357be817
pank5214 <pankajkumar5214@gmail.com>
2024-02-08 20:42:49 +0530

    implement infinite scrolling

--- src/utils/useVideosInfo.js
+++ src/utils/useVideosInfo.js
@@ -3,15 +3,41 @@ import { YOUTUBE_VIDEOS_API } from "./Constants";
 
 const useVideosInfo = () => {
   const [videos, setVideos] = useState([]);
+  const [pageToken, setPageToken] = useState("");
 
   useEffect(() => {
     getVideos();
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 1 >=
+          document.documentElement.scrollHeight &&
+        pageToken != null
+      ) {
+        return;
+      }
+      getVideos();
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [pageToken]);
+
   const getVideos = async () => {
-    const data = await fetch(YOUTUBE_VIDEOS_API);
-    const json = await data.json();
-    setVideos(json?.items);
+    try {
+      const url =
+        pageToken !== ""
+          ? `${YOUTUBE_VIDEOS_API}&pageToken=${pageToken}`
+          : YOUTUBE_VIDEOS_API;
+      const data = await fetch(url);
+      const json = await data.json();
+      console.log("json:", json);
+      setVideos((prevVideos) => [...prevVideos, ...json?.items]);
+      setPageToken(json?.nextPageToken || "");
+    } catch (e) {
+      console.error(e);
+    }
   };
   return videos;
 };
